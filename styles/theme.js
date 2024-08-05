356e4cf9c7e12c11c0592484983bf2efedb2f79c
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-08-05 16:38:31 -0300

    fix bug

--- styles/theme.js
+++ styles/theme.js
@@ -2,47 +2,17 @@ import { extendTheme } from '@chakra-ui/react'
 import { mode } from '@chakra-ui/theme-tools'
 
 const styles = {
-  global: props => ({
+  global: {
     body: {
       bg: "#EAE0D5"
     }
-  })
-}
-
-const components = {
-  Heading: {
-    variants: {
-      'section-title': {
-        textDecoration: 'underline',
-        fontSize: 20,
-        textUnderlineOffset: 6,
-        textDecorationColor: '#525252',
-        textDecorationThickness: 4,
-        marginTop: 3,
-        marginBottom: 4
-      }
-    }
-  },
-  Link: {
-    baseStyle: props => ({
-      color: "#3d7aed",
-      textUnderlineOffset: 3
-    })
   }
 }
 
-const fonts = {
-  heading: "'M PLUS Rounded 1c'"
-}
-
-const colors = {
-  grassTeal: '#88ccca'
-}
-
 const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: true
+  initialColorMode: 'light',
+  useSystemColorMode: false
 }
 
-const theme = extendTheme({ config, styles, components, fonts, colors })
+const theme = extendTheme({ config, styles })
 export default theme
