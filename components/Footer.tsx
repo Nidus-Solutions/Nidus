1dc7b411293ea54fbc407551ec36f7653dc3eed6
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-08-05 12:01:47 -0300

    refactor: Update Footer component to use Chakra UI color mode values

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
-import { Box } from '@chakra-ui/react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box alignItems="center" opacity={0.4} fontSize="sm">
+    <Box alignItems="center" opacity={useColorModeValue(0.7, 0.5)} fontSize="sm" color={useColorModeValue("gray.900", "gray.200")}>
       &copy; {new Date().getFullYear()} Nidus Solutions. All Rights Reserved.
     </Box>
   )
