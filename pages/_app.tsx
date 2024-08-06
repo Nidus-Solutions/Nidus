fa07b0d5d6b2691a0e22e5f10d71d3699d229f41
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-08-06 13:18:08 -0300

    Admin Painle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/main";
-import LayoutAdmin from "../components/admin/layout/mainAdmin";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/styles/theme";
 import Fonts from '../components/font';
@@ -9,15 +8,9 @@ export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      {router.pathname.includes("/admin")  ? (
-        <LayoutAdmin router={router}>
-          <Component {...pageProps} />
-        </LayoutAdmin>
-      ) : (
-        <Layout router={router}>
-          <Component {...pageProps} />
-        </Layout>
-      )}
+      <Layout router={router}>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   )
 }
--- pages/authentication.tsx
+++ pages/authentication.tsx
@@ -1,144 +1,100 @@
 import {
+    Flex,
     Box,
     FormControl,
-    Checkbox,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
     Stack,
     Button,
     Heading,
     Text,
+    useColorModeValue,
+    Link,
 } from '@chakra-ui/react'
-import { useCallback, useState } from 'react'
-import InputComponent from '@/components/Input';
-import axios from 'axios';
+import { useState } from 'react'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import Layout from '@/components/layout/article'
 
-const Authentication = () => {
-    const [inputs, setInputs] = useState({
-        email: "",
-        password: ""
-    });
-
-    const [status, setStatus] = useState({
-        submitted: false,
-        submitting: false,
-        info: { error: false, msg: null }
-    });
-
-    const handlOnChange = useCallback((e: { persist: () => void; target: { id: any; value: any; }; }) => {
-        e.persist();
-        setInputs((prev) => ({
-            ...prev,
-            [e.target.id]: e.target.value
-        }));
-        setStatus({
-            submitted: false,
-            submitting: false,
-            info: { error: false, msg: null }
-        });
-    }, []);
-
-    const handleServerResponse = useCallback((ok: any, msg: any) => {
-        if (ok) {
-            setStatus({
-                submitted: true,
-                submitting: false,
-                info: { error: false, msg }
-            });
-            setInputs({
-                email: "",
-                password: ""
-            });
-        } else {
-            setStatus({
-                submitted: false,
-                submitting: false,
-                info: { error: true, msg }
-            });
-        }
-    }, []);
-
-    const handleOnSubmit = useCallback((e: { preventDefault: () => void; }) => {
-        e.preventDefault();
-        setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
-        console.log("authenticating");
-        
-        const user = {
-            username: "admin",
-            email: "jbispo20@mail.com",
-            password: "123456",
-            urlImg: "https://avatars.githubusercontent.com/u/83095574?v=4",
-            company: "Nidus",
-        }
-
-        console.log(user);
-
-        localStorage.setItem("user", JSON.stringify(user));
-        window.location.href = "/admin";
-
-        handleServerResponse(true, "Login efetuado com sucesso");
-    }, []);
+export default function SignupCard() {
+    const [showPassword, setShowPassword] = useState(false)
 
     return (
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-            <Stack align={'center'}>
-                <Heading fontSize={'4xl'}>Painel do Cliente</Heading>
-            </Stack>
-            <Box
-                rounded={'lg'}
-                boxShadow={'lg'}
-                p={8}>
-                <Stack spacing={4}>
-                    <FormControl
-                        as='form'
-                        onSubmit={handleOnSubmit}
-                        display='flex'
-                        flexDirection='column'
-                        alignItems='center'
-                        justifyContent='center'
-                        gap={4}
-
-                    >
-                        <InputComponent
-                            id='email'
-                            type='email'
-                            value={inputs.email}
-                            onChange={handlOnChange}
-                            placeholder='Email'
-                            required
-                        />
-
-                        <InputComponent
-                            id='password'
-                            type='password'
-                            value={inputs.password}
-                            onChange={handlOnChange}
-                            placeholder='Senha'
-                            required
-                        />
-                        <Box
-                            display='flex'
-                            justifyContent='space-between'
-                            alignItems='center'
-                            w='100%'>
-                            <Checkbox defaultChecked>Manter conectado</Checkbox>
-                            <Text fontSize='sm' color="gray.800">
-                                Esqueceu sua senha?
-                            </Text>
-                        </Box>
-
-                        <Button
-                            type='submit'
-                            bg={'blue.400'}
-                            color={'white'}
-                            _hover={{
-                                bg: 'blue.500',
-                            }}>
-                            Acessar
-                        </Button>
-                    </FormControl>
+        <Layout title="Sign up">
+            <Flex
+                minH={'100vh'}
+                align={'center'}
+                justify={'center'}
+                bg={useColorModeValue('gray.50', 'gray.800')}>
+                <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                    <Stack align={'center'}>
+                        <Heading fontSize={'4xl'} textAlign={'center'}>
+                            Sign up
+                        </Heading>
+                        <Text fontSize={'lg'} color={'gray.600'}>
+                            to enjoy all of our cool features ✌️
+                        </Text>
+                    </Stack>
+                    <Box
+                        rounded={'lg'}
+                        bg={useColorModeValue('white', 'gray.700')}
+                        boxShadow={'lg'}
+                        p={8}>
+                        <Stack spacing={4}>
+                            <HStack>
+                                <Box>
+                                    <FormControl id="firstName" isRequired>
+                                        <FormLabel>First Name</FormLabel>
+                                        <Input type="text" />
+                                    </FormControl>
+                                </Box>
+                                <Box>
+                                    <FormControl id="lastName">
+                                        <FormLabel>Last Name</FormLabel>
+                                        <Input type="text" />
+                                    </FormControl>
+                                </Box>
+                            </HStack>
+                            <FormControl id="email" isRequired>
+                                <FormLabel>Email address</FormLabel>
+                                <Input type="email" />
+                            </FormControl>
+                            <FormControl id="password" isRequired>
+                                <FormLabel>Password</FormLabel>
+                                <InputGroup>
+                                    <Input type={showPassword ? 'text' : 'password'} />
+                                    <InputRightElement h={'full'}>
+                                        <Button
+                                            variant={'ghost'}
+                                            onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                                            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <Stack spacing={10} pt={2}>
+                                <Button
+                                    loadingText="Submitting"
+                                    size="lg"
+                                    bg={'blue.400'}
+                                    color={'white'}
+                                    _hover={{
+                                        bg: 'blue.500',
+                                    }}>
+                                    Sign up
+                                </Button>
+                            </Stack>
+                            <Stack pt={6}>
+                                <Text align={'center'}>
+                                    Already a user? <Link color={'blue.400'}>Login</Link>
+                                </Text>
+                            </Stack>
+                        </Stack>
+                    </Box>
                 </Stack>
-            </Box>
-        </Stack>
+            </Flex>
+        </Layout>
     )
 }
-
-export default Authentication
