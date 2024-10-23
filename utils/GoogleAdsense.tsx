204d6e79382ec655a8e208c319a5e2c4717645e8
João Gustavo Soares Bispo <83095574+jgb27@users.noreply.github.com>
2024-10-23 16:50:53 -0300

    Update GoogleAdsense.tsx

--- utils/GoogleAdsense.tsx
+++ utils/GoogleAdsense.tsx
@@ -5,15 +5,13 @@ type Props = {
 };
 
 const GoogleAdsense: React.FC<Props> = ({ pId }) => {
-  if (process.env.NODE_ENV !== "production") {
-    return null;
-  }
   return (
     <Script
       async
-      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+      src={`https://www.googletagmanager.com/gtag/js?id=-${pId}`}
       crossOrigin="anonymous"
       strategy="afterInteractive"
+
     />
   );
 };
