af533655c6f243fa59214eadfc85c14654de9451
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-10-24 11:13:53 -0300

    Refactor Footer component to include GitHub link
    This commit refactors the Footer component in the codebase to include a GitHub link. The link is displayed using the Chakra UI Link component and the GitHub icon from the react-icons library. The link is set to open in a new tab using the isExternal prop. This change enhances the user experience by providing a direct link to the GitHub repository of Nidus Solutions.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,24 @@
-import { Box, useColorModeValue } from '@chakra-ui/react'
+import { Box, useColorModeValue, Link, Icon } from '@chakra-ui/react'
+import { FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
   return (
     <Box
       alignItems="center"
+      justifyContent={{ base: 'center', md: 'space-between' }}
       opacity={useColorModeValue(0.7, 0.5)}
       fontSize="sm"
       mb={2}
       color="#F1C40F"
     >
-      &copy; {new Date().getFullYear()} Nidus Solutions. All Rights Reserved.
+      <Box>
+        &copy; {new Date().getFullYear()} Nidus Solutions | 57.581.208/0001-69
+        <Link href="https://github.com/Nidus-Solutions/Nidus" isExternal>
+          <Icon as={FaGithub} boxSize={5} ml={2} mb={-1} />
+        </Link>
+      </Box>
+
+
     </Box>
   )
 }
