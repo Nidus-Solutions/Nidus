ca83312253c2dc2a41193cb6f0faa9a16b509ef7
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-10-25 17:43:13 -0300

    Fé, que  a Tag vai

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -27,19 +27,17 @@ const Main = ({ children, router }: MainProps) => {
 
                 {/* Google Tag Manager Script */}
                 <Script
-                    id="google-tag-manager"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id=' + '${process.env.NEXT_PUBLIC_GTM_ID}';
-                        f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer');
-                    `,
-                    }}
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_MANAGER}`}
+                    strategy="afterInteractive" 
                 />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', ${process.env.GTAG_MANAGER});
+                    `}
+                </Script>
             </Head>
 
             <Navbar path={router.asPath} />
@@ -63,16 +61,6 @@ const Main = ({ children, router }: MainProps) => {
             >
                 <Footer />
             </Box>
-
-            {/* Google Tag Manager NoScript */}
-            <noscript>
-                <iframe
-                    src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-                    height="0"
-                    width="0"
-                    style={{ display: 'none', visibility: 'hidden' }}
-                ></iframe>
-            </noscript>
         </Box>
     )
 }
