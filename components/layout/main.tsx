a586937be773e2a5bcd7fb93501cc901ed34b77b
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-10-25 17:49:09 -0300

    Já não ligo para nada

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -1,12 +1,8 @@
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 import Head from "next/head";
-import dotenv from "dotenv";
-dotenv.config();
 import type { AppProps } from "next/app";
 import { Box, Container } from "@chakra-ui/react";
 import Navbar from "../navbar";
 import Footer from '../Footer'
-import Script from 'next/script';
 
 interface MainProps {
     children: React.ReactNode,
@@ -25,19 +21,28 @@ const Main = ({ children, router }: MainProps) => {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
 
-                {/* Google Tag Manager Script */}
-                <Script
-                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_MANAGER}`}
-                    strategy="afterInteractive" 
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', ${process.env.GTAG_MANAGER});
-                    `}
-                </Script>
+                {/* Google Tag Ads */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16751621578"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'AW-16751621578');
+                    `,
+                }} />
+
+                {/* Google Tag Analytics */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-5LKDGWPGEN"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                
+                    gtag('config', 'G-5LKDGWPGEN');
+                    `,
+                }} />
             </Head>
 
             <Navbar path={router.asPath} />
