7612d26d193dbd95e7a3860feaf9ca15f00d2740
João Gustavo Soares Bispo <83095574+jgb27@users.noreply.github.com>
2024-10-25 13:22:33 -0300

    Update main.tsx

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -34,7 +34,7 @@ const Main = ({ children, router }: MainProps) => {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', ${process.env.GTAG_MANAGER});
+                    gtag('config', ${process.env.NEXT_PUBLIC_GTAG_MANAGER});
                 `}
                 </Script>
             </Head>
